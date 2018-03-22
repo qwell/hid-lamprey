13c68b3ac668cca826f2ef83a29b473a66cbb92a
Jason Parker <north@ntbox.com>
2018-03-22 15:59:08 -0400

    Add a GTK button just for test purposes.

--- display-gtk.c
+++ display-gtk.c
@@ -4,12 +4,21 @@
 
 static void display_activate(GtkApplication *app, gpointer user_data) {
 	GtkWidget *window;
+	GtkWidget *button;
+	GtkWidget *button_box;
 
-	window = gtk_application_window_new (app);
-	gtk_window_set_title (GTK_WINDOW (window), "Window");
-	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+	window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "Window");
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-	gtk_widget_show_all (window);
+	button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+	gtk_container_add(GTK_CONTAINER(window), button_box);
+
+	button = gtk_button_new_with_label("Hello World");
+	g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+	gtk_container_add(GTK_CONTAINER(button_box), button);
+
+	gtk_widget_show_all(window);
 }
 
 void *hl_display_gtk(void *ptr) {
@@ -18,9 +27,9 @@ void *hl_display_gtk(void *ptr) {
 	int status;
 
 	app = gtk_application_new("com.ntbox.lamprey", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (display_activate), NULL);
-	status = g_application_run (G_APPLICATION (app), args->argc, args->argv);
-	g_object_unref (app);
+	g_signal_connect(app, "activate", G_CALLBACK(display_activate), NULL);
+	status = g_application_run(G_APPLICATION(app), args->argc, args->argv);
+	g_object_unref(app);
 
 	return status ? NULL : NULL;
 }
