637fbd5b5d0c29f7b1dcd2f7dbbbd7d8eb5e32bb
Jason Parker <north@ntbox.com>
2018-04-13 02:23:35 -0400

    Use libxml2 on Windows

--- include/config-win.h
+++ include/config-win.h
@@ -9,7 +9,7 @@
 #define HAVE_SYS_STAT_H
 #define HAVE_SYS_TYPES_H
 #define HAVE_UNISTD_H
-//#define HAVE_XML2
+#define HAVE_XML2
 #define HAVE__BOOL
 #define PACKAGE_NAME "lamprey"
 #define PACKAGE_VERSION "0.0.1"
