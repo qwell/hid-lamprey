98f6ca09034544d7df8b73a6d8c3c69f05f8ee87
Jason Parker <north@ntbox.com>
2019-01-25 00:56:05 -0500

    Add dummy FormDisplay

---
+++ FormDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lamprey
+{
+    public partial class FormDisplay : Form
+    {
+        public FormDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
