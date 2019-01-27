ac73b81b937f351ff1521e8015c60f9944b1062c
Jason Parker <north@ntbox.com>
2019-01-27 06:31:05 -0500

    Add an unknown category for input codes.

--- InputCode.cs
+++ InputCode.cs
@@ -6,10 +6,12 @@ namespace Lamprey
     {
         public enum InputCategory
         {
+            UnknownCategory,
             Button,
             AxisAbsolute,
             AxisRelative,
         }
+
         public enum InputCodeType
         {
             UnknownType,
@@ -22,7 +24,7 @@ namespace Lamprey
             RelativeAxis,
         }
 
-        public InputCategory Category { get; }
+        public InputCategory Category { get; } = InputCategory.UnknownCategory;
         public InputCodeType Type { get; } = InputCodeType.UnknownType;
         public int Code { get; }
         public String Description { get; }
